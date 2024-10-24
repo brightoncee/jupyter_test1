88b6f932846fe8947ff619119348ca81690cbc17
brightoncee <brighteegbara@gmail.com>
2024-10-24 09:32:28 +0100

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print ("Hello world!")
+
+
+# In[6]:
+
+
+print ("I you")
+
+
+# In[1]:
+
+
+3+7
+
+
+# In[2]:
+
+
+#calculating michealis mentain kinetics
+km=15.0      #km =15 micromolar
+vmax=100.0   #vmax=10.0 nanomoles/sec
+s_conc =8.0
+velocity = vmax * s_conc / (km + s_conc)
+
+
+# In[3]:
+
+
+print(velocity)
+
+
+# In[4]:
+
+
+print(velocity)
+velocity*1000
+print(velocity)
+
+
+# In[2]:
+
+
+print("this is my first python code")
+
+
+# In[1]:
+
+
+print("hello_world")
+
+
+# In[3]:
+
+
+#using pythn as a calculator
+2+2
+
+
+# In[5]:
+
+
+(50 -5*6)/4
+
+
+# In[7]:
+
+
+17//3
+
+
+# In[12]:
+
+
+tax =12.5/ 100
+
+
+# In[13]:
+
+
+tax
+
+
+# In[14]:
+
+
+price=100.5
+
+
+# In[15]:
+
+
+price*tax
+
+
+# In[16]:
+
+
+round(_, 2)
+
+
+# In[19]:
+
+
+#strings
+
+print('c:\some\name')
+
+
+# In[20]:
+
+
+print(r'c:\some\name')
+
+
+# In[22]:
+
+
+print("hello,world")
+
+
+# In[ ]:
+
+
+
+
